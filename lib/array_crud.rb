d1afed2f5d2a66ec6c26d56fa90043a1e2d6c565
Mikael Kaya <kayaswork@gmail.com>
2019-12-21 21:59:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array
   ["Honda"]
   add_element_to_end_of_array << "Nissan"
-  puts add_element_to_end_of_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
